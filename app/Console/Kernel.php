0fa33ebb43b125a471b9b02581604dfb79a17c81
NowNewStart <j.gelmar@hotmail.de>
2018-06-14 14:05:59 +0200

    fix typo

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        \App\COnsole\Commands\StandingsCommand::class,
+        \App\Console\Commands\StandingsCommand::class,
     ];
 
     /**
