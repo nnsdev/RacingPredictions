90162ac8208e3188e57d0d899597534c4fee212a
NowNewStart <j.gelmar@hotmail.de>
2018-06-16 23:31:30 +0200

    I like moving stuff around

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,7 +1,7 @@
 @extends('layout.app')
 
 @section('content')
-<div class="container-lg py-5">
+<div class="container-lg py-3">
     <div class="row px-4">
         <div class="col-md-12">
             @if($errors->any())
@@ -14,7 +14,7 @@
         </div>
     </div>
     <div class="row">
-        <div class="col-md-4 px-5">
+        <div class="col-md-6 px-5">
             @if(now()->lessThan(\Carbon\Carbon::parse("2018-06-16T13:00:00Z")))
             <div class="card">
                 <div class="card-header">
@@ -78,25 +78,34 @@
                     </form>
                 </div>
             </div>
+            @else
+            <div class="card">
+                <div class="card-header">
+                    Top 10
+                </div>
+                <div class="card-body">
+                    <leaderboard :userid="{{ Auth::user()->id }}"></leaderboard>
+                </div>
+            </div>
             @endif
-            <div class="card mt-2">
+            <div class="card mt-1">
                 <div class="card-header">Most favorite teams</div>
                 <div class="card-body">
                     @if($most_picked['lmp1'])
                     <div class="row">
-                        <div class="col-md-6">
+                        <div class="col-md-3">
                             <h3><span class="badge badge-danger">LMP1</span></h3>
                             <h4>#{{ $most_picked['lmp1']->car_number  }} {{ $most_picked['lmp1']->name }}</h4>
                         </div>
-                        <div class="col-md-6">
+                        <div class="col-md-3">
                             <h3><span class="badge badge-danger">LMP2</span></h3>
                             <h4>#{{ $most_picked['lmp2']->car_number  }} {{ $most_picked['lmp2']->name }}</h4>
                         </div>
-                        <div class="col-md-6 mt-4">
+                        <div class="col-md-3">
                             <h3><span class="badge badge-danger">GTE Pro</span></h3>
                             <h4>#{{ $most_picked['gtepro']->car_number  }} {{ $most_picked['gtepro']->name }}</h4>
                         </div>
-                        <div class="col-md-6 mt-4">
+                        <div class="col-md-3">
                             <h3><span class="badge badge-danger">GTE Am</span></h3>
                             <h4>#{{ $most_picked['gteam']->car_number  }} {{ $most_picked['gteam']->name }}</h4>
                         </div>
@@ -105,8 +114,8 @@
                 </div>
             </div>
         </div>
-        <div class="col-md-8 px-5">
-            <div class="card mt-2">
+        <div class="col-md-6 px-5">
+            <div class="card">
                 <div class="card-header">Search for User</div>
                 <div class="card-body">
                     @if(Session::has('error'))
@@ -126,24 +135,15 @@
                 </div>
             </div>
             @if(now()->greaterThan(\Carbon\Carbon::parse("2018-06-16T13:00:00Z")) && now()->lessThan(\Carbon\Carbon::parse("2018-06-17T13:30:00Z")))
-            <div class="card mt-2">
+            <div class="card mt-1">
                 <div class="card-header">Live Data</div>
                 <div class="card-body">
                     <standings></standings>
                 </div>
             </div>
             @endif
-            @if(now()->greaterThan(\Carbon\Carbon::parse("2018-06-16T13:00:00Z")))
-            <div class="card mt-2">
-                <div class="card-header">
-                    Top 10
-                </div>
-                <div class="card-body">
-                    <leaderboard :userid="{{ Auth::user()->id }}"></leaderboard>
-                </div>
-            </div>
-            @elseif(now()->lessThan(\Carbon\Carbon::parse("2018-06-16T13:00:00Z")))
-            <div class="card mt-2">
+            @if(now()->lessThan(\Carbon\Carbon::parse("2018-06-16T13:00:00Z")))
+            <div class="card mt-1">
                 <div class="card-header">Latest 5 Picks</div>
                 <div class="card-body">
                     <table class="table">
